db69967d90765b92504dd95c4c44497d538077fe
Roger Day <notsofeo@gmail.com>
2014-06-10 21:51:19 -0400

    saving V_toxDoseThreshold

---
+++ V_toxDoseThreshold_Roger_1402430209.97258.R
@@ -0,0 +1,8 @@
+new("Variable"
+    , name = "V_toxDoseThreshold_Roger_1402430209.97258"
+    , description = "dose threshold for binary toxicity event"
+    , checkDataType = function(x)is.double(x)&(x>=0)
+    , author = "Roger"
+    , timestamp = structure(1402451439.55507, class = c("POSIXct", "POSIXt"))
+    , filename = "V_toxDoseThreshold_Roger_1402430209.97258.R"
+)
