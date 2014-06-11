403b23e22356a9dbcb114ac8a1a0c1701bd05b3b
Roger Day <notsofeo@gmail.com>
2014-06-10 21:51:55 -0400

    saving V_SampleSizeMaxIsReached

---
+++ V_SampleSizeMaxIsReached_Roger_1402430210.01666.R
@@ -0,0 +1,8 @@
+new("Variable"
+    , name = "V_SampleSizeMaxIsReached_Roger_1402430210.01666"
+    , description = "Upper bound for sample size has been reached."
+    , checkDataType = .Primitive("is.logical")
+    , author = "Roger"
+    , timestamp = structure(1402451507.07234, class = c("POSIXct", "POSIXt"))
+    , filename = "V_SampleSizeMaxIsReached_Roger_1402430210.01666.R"
+)
