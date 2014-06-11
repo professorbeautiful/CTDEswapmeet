0530411c06e1212992656135fe33e9ac65d92ead
Roger Day <notsofeo@gmail.com>
2014-06-10 21:51:29 -0400

    saving V_dose

---
+++ V_dose_Roger_1402430210.01305.R
@@ -0,0 +1,8 @@
+new("Variable"
+    , name = "V_dose_Roger_1402430210.01305"
+    , description = "dose"
+    , checkDataType = .Primitive("is.numeric")
+    , author = "Roger"
+    , timestamp = structure(1402451479.9976, class = c("POSIXct", "POSIXt"))
+    , filename = "V_dose_Roger_1402430210.01305.R"
+)
