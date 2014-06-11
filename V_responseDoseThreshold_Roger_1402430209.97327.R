7161f861afec88d947ac8a3ed57d7571b0ea47f2
Roger Day <notsofeo@gmail.com>
2014-06-10 21:50:39 -0400

    saving V_responseDoseThreshold

---
+++ V_responseDoseThreshold_Roger_1402430209.97327.R
@@ -0,0 +1,8 @@
+new("Variable"
+    , name = "V_responseDoseThreshold_Roger_1402430209.97327"
+    , description = "dose threshold for binary response event"
+    , checkDataType = function(x)is.double(x)&(x>=0)
+    , author = "Roger"
+    , timestamp = structure(1402451419.52871, class = c("POSIXct", "POSIXt"))
+    , filename = "V_responseDoseThreshold_Roger_1402430209.97327.R"
+)
